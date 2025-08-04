88c00d5257aab51e2caf4e53e942fb0a30979fdf
Deep Raj <deepraj6897@gmail.com>
2025-08-04 17:34:40 +0530

    home section layout is set

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 
+import { Button } from '@/components/ui/button'
+import  Link from 'next/link'
+import {  ArrowRightIcon } from 'lucide-react'
 import React from 'react'
+import TerminalOverlay from '@/components/ui/TerminalOverlay'
 
 const HomePage = () => {
   return (
@@ -7,7 +11,9 @@ const HomePage = () => {
       <section className='relative z-10 py-24 flex-grow'>
         <div className='container mx-auto px-4'>
            <div className='grid grid-cols-1 lg:grid-cols-12 gap-8 items-center relative'>
-             <div className='absolute -top-10 left-0 2-40 h-40 border-l-2 border-t-2 border-border'>
+            {/**Corner Decoration  */}
+             <div className='absolute -top-6 left-0 w-15 h-15 border-l-2 border-t-2 border-border'/>
+              
                <div className='lg:col-span-7 space-y-8 relative'>
                  <h1 className='text-5xl md:text-6xl lg:text-7xl font-bold tracking-light'>
                   <div>
@@ -50,9 +56,49 @@ const HomePage = () => {
                         <div className='text-xs uppercase tracking-wider'>PERSONALIZED</div>
                       </div>
                  </div>
+
+                 {/** button section*/}
+
+                 <div className='flex flex-col sm:flex-row gap-4 pt-6'>
+                   <Button size="lg" asChild className="overflow-hidden bg-primary text-primary-foreground px-8 py-6 text-lg font-medium">
+                     <Link href={"/generate-program"} className="flex items-center font-mono">
+                      Build Your Program
+                      <ArrowRightIcon className='ml-2 size-5'/>
+                     </Link>
+                   </Button>
+                 </div>
                </div>
 
-             </div>
+               <div className='lg:col-span-5 relative'>
+                 <div className='absolute -inset-4 pointer-events-none'>
+                   <div className='absolute top-0 left-0 w-16 h-16 border-l-2 border-t-2 border-border'/>
+                    <div className='absolute top-0 right-0 w-16 h-16 border-r-2 border-t-2 border-border'/>
+                    <div className='absolute bottom-0 left--0 w-16 h-16 border-l-2 border-b-2 border-border'/>
+                    <div className='absolute bottom-0 right-0 w-16 h-16 border-r-2 border-b-2 border-border'/>
+                 </div>
+
+                 <div className='relative aspect-square max-w-lg mx-auto'>
+                   <div className='relative overflow-hidden rounded-lg bg-black'></div>
+                      <img src="/fitness.jpg" alt="AI coach" className='size-full object-cover object-center'/>
+                  </div>
+                   <div className="absolute inset-0 bg-[linear-gradient(transparent_0%,transparent_calc(50%-1px),var(--glow-primary)_50%,transparent_calc(50%+1px),transparent_100%)] bg-[length:100%_8px] animate-scanline pointer-events-none" />
+                  
+                  <div className='absolute inset-0 pointer-events-none'>
+                     <div className='absolute top-1/3 left-1/3 w-1/3 h-1/3 border border-primary/40 rounded-full'/>
+
+                     <div className='absolute top-1/2 left-0 w-1/4 h-px bg-primary/50 opacity-70'/>
+                     <div className='absolute top-1/2 right-0 w-1/4 h-px bg-primary/50 opacity-70'/>
+                     <div className='absolute top-0 left-1/2 h-1/4 w-px bg-primary/50 opacity-70'/>
+                     <div className='absolute bottom-0 left-1/2 h-1/4 w-px bg-primary/50 opacity-70'/>
+
+                  </div>
+
+                  <div className='absolute inset-0 bg-gradient-to-t from-background via-background/40 to-transparent'/>
+                    {/**terminal overlay */}
+                <TerminalOverlay/> 
+                </div>
+              
+               
            </div>
         </div>
       </section>
